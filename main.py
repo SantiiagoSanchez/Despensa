116cdae587ccb00c4941c357bb17b23acfa76020
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-08-11 19:09:40 -0300

    Post de productos funcionando

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from routers import productos
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 from bd.client import db
 
 
 
 app = FastAPI()
+
+
+#fastapi dev main.py Arrancar servidor
+
 
 # 1. Configurar templates
 templates = Jinja2Templates(directory="templates")
@@ -21,6 +26,29 @@ def lista_prod(request: Request):
         "producto/lista.html",
         {"request": request, "productos": productos_list}
     )
+
+@app.get("/productos/nuevo")
+def nuevo_producto(request: Request):
+    return templates.TemplateResponse("producto/nuevo.html", {"request": request})
+
+@app.post("/productos")
+def crear_producto(
+    request: Request,
+    nombre: str = Form(...),
+    descripcion: str = Form(None),
+    precio: float = Form(...),
+    stock: int = Form(...),
+    activo: bool = Form(False)
+):
+    nuevo = {
+        "nombre": nombre,
+        "descripcion": descripcion,
+        "precio": precio,
+        "stock": stock,
+        "activo": activo,
+    }
+    db.productos.insert_one(nuevo)
+    return RedirectResponse(url="/productos/lista", status_code=303)
 
 # 3. Incluir router después
 app.include_router(productos.router)
